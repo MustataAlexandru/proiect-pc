93f3b8ffabaf33370a8be9a7889e03889e6a615c
Alexandru-Cristian Mustata <106619900+MustataAlexandru@users.noreply.github.com>
2025-01-15 21:10:25 +0200

    refactor user data / ui improvements

---
+++ app/posts/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingContainer from '@/app/global/LoadingContainer'
+import React from 'react'
+
+function loading() {
+  return (
+    <div className='pt-36 pb-36'>
+    <LoadingContainer />
+    </div>
+  )
+}
+
+export default loading
