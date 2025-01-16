b176e1305331a7472f44f3aa5a5b1c5b1d721498
Alexandru-Cristian Mustata <106619900+MustataAlexandru@users.noreply.github.com>
2025-01-16 18:58:29 +0200

    fixed search bug

--- app/navbar/Navbar.tsx
+++ app/navbar/Navbar.tsx
@@ -6,20 +6,18 @@ import UserDropdown from "./UserDropdown";
 import NavSearch from "./NavSearch";
 import { currentUser } from "@clerk/nextjs/server";
 
-
 export default async function Navbar() {
   const user = await currentUser();
   return (
     <div className="flex items-center justify-between py-4 px-4 border-b fixed top-0 w-[100%] z-10 bg-white dark:bg-[hsl(var(--background))]">
       <div className="flex items-center gap-2">
         <Logo />
-        <NavMenu />
+        {/* <NavMenu /> */}
       </div>
-      <div className="flex-1 flex justify-center lg:mr-40">
+      <div className="w-96">
         <NavSearch />
       </div>
       <div className="flex items-center gap-2">
-      
         <DarkMode />
         <UserDropdown />
       </div>
