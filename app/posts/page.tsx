0ecac045781c676006b6261ad5f35b55d7705bc3
Alexandru-Cristian Mustata <106619900+MustataAlexandru@users.noreply.github.com>
2025-01-16 18:49:49 +0200

    fixed search bug

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,7 +5,7 @@ import PostCard from "./PostCard";
 export default async function AllPosts({searchParams = {}} : {searchParams: {search?: string}}) {
   const search = searchParams?.search || '';
   const posts = await fetchAllPosts({search});
-  if(posts.length === 0) return <p> No properties yet. </p>
+  if(posts.length === 0) return <p className="pt-36 pb-36"> No properties matched your search. </p>
   return <section className="pt-36 pb-12 grid gap-12 md:grid-cols-2 lg:grid-cols-3">
     {posts.map((post) => {return <PostCard key={post.id} postInfo={post} />})}
   </section>
